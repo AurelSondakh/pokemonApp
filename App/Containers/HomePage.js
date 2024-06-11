bb934324de00e49bd97df7523a08051ca90f1eca
aurelsondakh <aurelsondakh@gmail.com>
2024-06-11 19:34:04 +0800

    feat: HomePage Tab View

--- App/Containers/HomePage.js
+++ App/Containers/HomePage.js
@@ -1,13 +1,60 @@
-import React from "react";
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Dimensions, TouchableOpacity } from 'react-native';
+import { TabView } from 'react-native-tab-view';
+import FavoritesScreen from '../Components/FavoritesScreen';
+import ExploreScreen from '../Components/ExploreScreen';
 
 const HomePage = () => {
-    return (
-        <View>
-            <Text style={{ fontFamily: 'Poppins-Bold' }}>THIS IS HOME PAGE</Text>
-            <Text>THIS IS HOME PAGE</Text>
-        </View>
-    )
-}
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    { key: 'explore', title: 'Explore' },
+    { key: 'favorites', title: 'Favorites' },
+  ]);
 
-export default HomePage
+  const TabBarComponent = (props) => {
+    return (
+      <View style={{
+        flexDirection: 'row'
+      }}>
+        {props.navigationState.routes.map((route, i) => {
+          return (
+            <TouchableOpacity
+              key={`index-tabbar-${i}`}
+              style={{
+                flex: 1,
+                alignItems: 'center',
+                padding: 14,
+                borderBottomWidth: 3,
+                borderBottomColor: index === i ? '#F26A24' : '#E2E4E7'
+              }}
+              onPress={() => {
+                setIndex(i)
+              }}>
+              <Text style={{ color: index === i ? '#F26A24' : '#80838C', fontFamily: 'Poppins-Medium' }}>{route.title}</Text>
+            </TouchableOpacity>
+          )
+        })}
+      </View>
+    )
+  }
+
+  return (
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={({ route }) => {
+        switch (route.key) {
+          case 'explore':
+            return <ExploreScreen />
+          case 'favorites':
+            return <FavoritesScreen />
+          default: return null
+        }
+      }}
+      onIndexChange={setIndex}
+      initialLayout={{ width: Dimensions.get('screen').width, height: Dimensions.get('screen').height }}
+      renderTabBar={TabBarComponent}
+    />
+  );
+};
+
+export default HomePage;
